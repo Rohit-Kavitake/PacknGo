0898c2ba8e7bb3e3841878a3be8acbf452ac1fa2
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2021-09-04 22:46:19 +0530

    added config file -connected mongo

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 // PacknGo Main server
 // Developer :- Rohit Kavitake
 
+// const dotenv = require('dotenv');
 const app = require('./app');
+
+const mongoose = require('mongoose');
+const DB = '' + process.env.database;
+mongoose
+    .connect(
+        'mongodb+srv://RohitKavitake:676cQ9R1Nn3nYyiV@cluster0.omqxr.mongodb.net/PacknGO?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+        }
+    )
+    .then(() => console.log('Database connected Successfully!'))
+    .catch((err) => console.log('Error Connecting Mongo : ' + err));
 
 const PORT = 8080;
 app.listen(PORT, () => {
-    console.log(
-        `PacknGo Server Is Running on Port : ${PORT}`
-    );
+    console.log(`PacknGo Server Is Running on Port : ${PORT}`);
 });
