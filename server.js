7269ef3def5eb7752cceaa4d285be199730f47e3
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2021-09-05 13:39:12 +0530

    commenting the code

--- server.js
+++ server.js
@@ -4,9 +4,12 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 
+//Configuring Environment variables
 dotenv.config({ path: './config.env' });
 const mongoose = require('mongoose');
 const DB = '' + process.env.DATABASE;
+
+//Connection to mongo Database
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
@@ -14,6 +17,7 @@ mongoose
     .then(() => console.log('Database connected Successfully!'))
     .catch((err) => console.log('Error Connecting Mongo : /n ' + err));
 
+//Starting the server
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`PacknGo Server Is Running on Port : ${PORT}`);
