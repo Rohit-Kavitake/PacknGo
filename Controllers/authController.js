2a82e4065cc624ded61a035b07aaa7fbbf13c8df
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2021-10-17 16:11:50 +0530

    typo fixed

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -12,7 +12,7 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
-    const cookieoptions = {
+    const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 3600 * 1000),
         secure: false,
         httpOnly: true,
